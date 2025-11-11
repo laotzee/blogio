cbd90654e224ed66aeb29fc04cf1d5c2a9537c5d
laotzee <laotzeabreu@proton.me>
2025-11-11 18:16:46 -0500

    Add script for initiating pipeline folders

---
+++ scripts/initial_setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+mkdir -p resources/background/used
+mkdir -p resources/factory_background
+mkdir -p resources/fonts/DMSerif
+mkdir -p resources/fonts/roboto
+mkdir -p resources/posts/en/used
+mkdir -p resources/posts/es/used
+mkdir -p resources/writing/es/used
+mkdir -p resources/writing/en/used
+
+mkdir data
