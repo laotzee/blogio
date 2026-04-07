52ebd4054560dddcf9d0b709496bc80170444520
laotzee <laotzeabreu@proton.me>
2026-04-07 12:27:15 -0400

    Include cocurrency on API calls

--- src/core/extractor.py
+++ src/core/extractor.py
@@ -1,8 +1,8 @@
 import os
-import json
 import shutil
 import sys
 import frontmatter
+import asyncio
 from google import genai
 from google.genai.types import Schema, Type
 from dotenv import load_dotenv
@@ -16,8 +16,11 @@ prompt = os.getenv("PROMPT")
 
 INPUT_FILE_PATH = os.path.join("resources/writing/", source)
 USED_FILES_PATH = os.path.join("resources/writing/", source, "used")
-
+MAX_CONCURRENT_REQUESTS = 5
+MAX_POST_PER_BATCH = 15
 current_model = "gemini-3-flash-preview"
+
+semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
 
 response_schema = Schema(
     type=Type.ARRAY,
@@ -37,50 +40,73 @@ def read_post(file_path: str) -> str:
     return blog_text
 
 
-def extract_quotes(prompt, content, model):
+async def extract_quotes(prompt, content, model, client):
 
-    client = genai.Client()
-
-    full_prompt = (
-        f"{prompt}\n\n--- BLOG POST START ---\n{content}\n--- BLOG POST END ---"
-    )
-
-    print("Calling Gemini API to extract quotes...")
-
-    try:
-        response = client.models.generate_content(
-            model=model,
-            contents=full_prompt,
-            config={
-                "response_mime_type": "application/json",
-                "response_schema": response_schema,
-            },
+    async with semaphore:
+        full_prompt = (
+            f"{prompt}\n\n--- BLOG POST START ---\n{content}\n--- BLOG POST END ---"
         )
 
-    except Exception as e:
-        print(f"An error occurred during the API call: {e}")
-        exit()
-    print("API call successful. Processing response...")
-    return response
+        print("Calling Gemini API to extract quotes...")
+
+        try:
+            response = await client.aio.models.generate_content(
+                model=model,
+                contents=full_prompt,
+                config={
+                    "response_mime_type": "application/json",
+                    "response_schema": response_schema,
+                },
+            )
+
+        except Exception as e:
+            print(f"An error occurred during the API call: {e}")
+            return None
+        print("API call successful. Processing response...")
+        return response
 
 
-def process_json(response) -> list[str]:
+def process_response(response) -> list[str] | None:
+    if response is None:
+        return None
+
     try:
-        quotes_list = json.loads(response.text)
-    except json.JSONDecodeError:
-        print("Error: Could not decode JSON response from the API.")
-        print("Raw API Response Text:", response.text)
-        exit()
+        return response.parsed
 
-    return quotes_list
+    except Exception as e:
+        print(f"Error: Could not extract parsed data. {e}")
+        return None
+
+
+async def main():
+    tasks = []
+    data = []
+    client = genai.Client()
+    post_files = get_input_files(INPUT_FILE_PATH)
+
+    for post in post_files[:MAX_POST_PER_BATCH]:
+        post_path = os.path.join(INPUT_FILE_PATH, post)
+        post = frontmatter.load(post_path)
+        tasks.append(extract_quotes(prompt, post.content, current_model, client))
+        post["path"] = post_path
+        data.append(post)
+
+    results = await asyncio.gather(*tasks)
+
+    for result, post in zip(results, data):
+        try:
+            quote_list = process_response(result)
+            if quote_list:
+                save_quotes(quote_list, post["title"], lang)
+
+                if os.path.exists(post["path"]):
+                    shutil.move(post["path"], USED_FILES_PATH)
+            else:
+                print(f"Skipping {post['title']}: No quotes extracted.")
+        except Exception as e:
+            print(f"Failed to process {post['title']}: {e}")
+            continue
 
 
 if __name__ == "__main__":
-    post_files = get_input_files(INPUT_FILE_PATH)
-    for post in post_files:
-        post_path = os.path.join(INPUT_FILE_PATH, post)
-        post = frontmatter.load(post_path)
-        response = extract_quotes(prompt, post.content, current_model)
-        quote_list = process_json(response)
-        save_quotes(quote_list, post["title"], lang)
-        shutil.move(post_path, USED_FILES_PATH)
+    asyncio.run(main())
